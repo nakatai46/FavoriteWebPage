2867b882517849feccdececc3cb342261f6d891b
nakatai46 <g018c1139+1@gmail.com>
2021-10-31 01:37:28 +0900

    :ambulance:コードの修正

--- register.php
+++ register.php
@@ -1,35 +1,4 @@
-<?php session_start();
-if( isset($_SESSION['user']) != "") {
-  // ログイン済みの場合はリダイレクト
-  header("Location: index.php");
-}
-// DBとの接続
-include_once 'dbconnect.php';
-
-// signupがPOSTされたときに下記を実行
-if(isset($_POST['signup'])) {
-
-    $username = $mysqli->real_escape_string($_POST['username']);
-    $email = $mysqli->real_escape_string($_POST['email']);
-    $password = $mysqli->real_escape_string($_POST['password']);
-    $password = password_hash($password, PASSWORD_DEFAULT);
-  
-    // POSTされた情報をDBに格納する
-    $query = "INSERT INTO users(username,email,password) VALUES('$username','$email','$password')";
-  
-    if($mysqli->query($query)) {  ?>
-<div class="alert alert-success" role="alert">登録しました</div>
-<?php } else { ?>
-<div class="alert alert-danger" role="alert">エラーが発生しました。</div>
-<?php
-    }
-  }
-
-?>
-
-
-
-<!DOCTYPE HTML>
+ <!DOCTYPE HTML>
 <html lang="ja">
 
 <head>
